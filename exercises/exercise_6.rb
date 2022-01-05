c6f4974c3b41dc7a0111777df4b365a2c1d4c8ea
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-05 18:42:58 -0500

    Add data using reference variables and connect employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "James", last_name: "Bond", hourly_rate: 69)
+@store1.employees.create(first_name: "Clara", last_name: "Klaus", hourly_rate: 20)
+@store1.employees.create(first_name: "Zoe", last_name: "Mills", hourly_rate: 49)
+
+@store2.employees.create(first_name: "Sally", last_name: "Watson", hourly_rate: 54)
+@store2.employees.create(first_name: "Philip", last_name: "Dexter", hourly_rate: 24)
+@store2.employees.create(first_name: "Morgan", last_name: "Dennis", hourly_rate: 30)
+@store2.employees.create(first_name: "Anna", last_name: "Smith", hourly_rate: 740)
